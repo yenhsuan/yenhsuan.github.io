604ae696193eae14c4901e4f712a411a6cdf3b39
yenhsuan <yenhsuan.terry@gmail.com>
2019-07-17 02:00:51 -0700

    add sass support, json as data model

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const ROOT_PATH = path.resolve(__dirname, './src');
+
 module.exports = {
   siteMetadata: {
     title: `Terry Chen | Software Engineer`,
@@ -5,12 +8,20 @@ module.exports = {
     author: `Terry Chen`,
   },
   plugins: [
+    `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${ROOT_PATH}/data/`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${ROOT_PATH}/images`,
       },
     },
     `gatsby-transformer-sharp`,
