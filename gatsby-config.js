d8577a2ea1089731eaca319dcfb86f9f8c9929a5
yenhsuan <yenhsuan.terry@gmail.com>
2019-07-22 22:41:26 -0700

    refactor airbnb styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,28 +3,28 @@ const ROOT_PATH = path.resolve(__dirname, './src');
 
 module.exports = {
   siteMetadata: {
-    title: `Terry Chen | Software Engineer`,
-    description: ``,
-    author: `Terry Chen`,
+    title: 'Terry Chen | Software Engineer',
+    description: '',
+    author: 'Terry Chen',
   },
   plugins: [
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-transformer-json`,
+    'gatsby-plugin-sass',
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-json',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
         path: `${ROOT_PATH}/data/`,
       },
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${ROOT_PATH}/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
   ],
 }
